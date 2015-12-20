6224673e76d3ba70c1b61c1d0b451bd4ec18632a
Colin Moore <colin.moore42@googlemail.com>
2015-12-20 21:06:44 +0000

    day 19 pt 2

---
+++ day19pt2.js
@@ -0,0 +1,61 @@
+
+
+// https://github.com/xPaw/adventofcode-solutions/blob/master/js/day19.js
+
+var fs = require('fs');
+
+var input = fs.readFileSync('day19.txt', 'utf8');
+
+	input = input.split( '' ).reverse().join( '' ).split( '\n' );
+	
+	var molecule = input.shift();
+	input.shift(); // empty line
+	
+	input = input.map( function( a )
+	{
+		return a.split( ' >= ' );
+	} );
+	
+	var uniqueNewMolecules = {};
+	var i, y;
+	
+	for( i = 0; i < molecule.length; i++ )
+	{
+		for( y = 0; y < input.length; y++ )
+		{
+			if( molecule.substr( i, input[ y ][ 1 ].length ) === input[ y ][ 1 ] )
+			{
+				uniqueNewMolecules[ molecule.slice( 0, i ) + input[ y ][ 0 ] + molecule.slice( i + input[ y ][ 1 ].length ) ] = true;
+			}
+		}
+	}
+	
+	var steps = 0;
+	var lookup = {};
+	var replaced = false;
+	
+	input = input.map( function( a )
+	{
+		lookup[ a[ 0 ] ] = a[ 1 ];
+		
+		return a[ 0 ];
+	} );
+	
+	var regexp = new RegExp( '(' + input.join( '|' ) + ')', 'g' );
+	var replaceCallback = function( matched )
+	{
+		replaced = true;
+		
+		steps++;
+		
+		return lookup[ matched ];
+	};
+	
+	do
+	{
+		replaced = false;
+		molecule = molecule.replace( regexp, replaceCallback );
+	}
+	while( replaced );
+	
+	console.log ( [Object.keys( uniqueNewMolecules ).length, steps ]);
